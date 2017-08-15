2eb040f150a1b0bbc50049d783188c7172f92320
Larry <larryjamestaylor@gmail.com>
2017-08-15 09:40:47 -0700

    add markDone method

--- app/to-do.js
+++ app/to-do.js
@@ -4,12 +4,16 @@ var Task = (function () {
         this.priority = priority;
         this.done = false;
     }
+    Task.prototype.markDone = function () {
+        this.done = true;
+    };
     return Task;
 }());
 var tasks = [];
 tasks.push(new Task('take out the trash', 'medium'));
 tasks.push(new Task('do dishes', 'high'));
 tasks.push(new Task('wash car', 'low'));
+tasks[1].markDone();
 for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
     var task = tasks_1[_i];
     console.log(task);
--- app/to-do.ts
+++ app/to-do.ts
@@ -3,12 +3,19 @@ class Task {
 
 
   constructor( public description: string, public priority: string){ }
+
+  markDone() {
+    this.done = true;
+  }
 }
 
 var tasks: Task[] = [];
 tasks.push(new Task('take out the trash', 'medium'));
 tasks.push(new Task('do dishes', 'high'));
 tasks.push(new Task('wash car', 'low'));
+
+
+tasks[1].markDone();
 
 for (var task of tasks){
 console.log(task);
