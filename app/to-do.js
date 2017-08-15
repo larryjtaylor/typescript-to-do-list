a4bea7cca95d5f5fb707ccfcbbb58c7c92b4efee
Larry <larryjamestaylor@gmail.com>
2017-08-15 09:38:37 -0700

    add for loop to show tasks in console

--- app/to-do.js
+++ app/to-do.js
@@ -1,11 +1,16 @@
 var Task = (function () {
-    function Task(descriptionParameter, priorityParameter) {
+    function Task(description, priority) {
+        this.description = description;
+        this.priority = priority;
         this.done = false;
-        this.description = descriptionParameter;
-        this.priority = priorityParameter;
     }
     return Task;
 }());
 var tasks = [];
 tasks.push(new Task('take out the trash', 'medium'));
-console.log(tasks);
+tasks.push(new Task('do dishes', 'high'));
+tasks.push(new Task('wash car', 'low'));
+for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
+    var task = tasks_1[_i];
+    console.log(task);
+}
--- app/to-do.ts
+++ app/to-do.ts
@@ -2,10 +2,14 @@ class Task {
   done: boolean = false;
 
 
-  constructor( public description: string, public priority: string){
-  }
+  constructor( public description: string, public priority: string){ }
 }
 
 var tasks: Task[] = [];
 tasks.push(new Task('take out the trash', 'medium'));
-console.log(tasks);
+tasks.push(new Task('do dishes', 'high'));
+tasks.push(new Task('wash car', 'low'));
+
+for (var task of tasks){
+console.log(task);
+}
